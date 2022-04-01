583352e81b2f3912d240bc4c5d577cb01670c7ad
Jacopo Di Ventura <jacopo.diventura@gmail.com>
2022-04-01 12:59:41 +0200

    Initial commit

---
+++ my_make_blobs.py
@@ -0,0 +1,191 @@
+from hashlib import new
+from multiprocessing import reduction
+from multiprocessing.sharedctypes import Value
+from tokenize import cookie_re
+import numpy as np
+import numbers
+from collections.abc import Iterable
+import scipy.spatial as spatial
+import miniball
+import math
+import heapq
+
+# https://github.com/scikit-learn/scikit-learn/blob/37ac6788c/sklearn/datasets/_samples_generator.py#L792
+
+
+def my_make_blobs(n_samples=100, n_features=2, centers=None, cluster_std=1.0, center_box=(-10.0, 10.0), return_centers=False):
+
+    # Uncomment for reproducible results, NOT WORKING
+    # np.random.seed(2022)
+
+    rng = np.random.default_rng(2022)
+
+    if isinstance(n_samples, numbers.Integral):
+        if centers is None:
+            centers = 3
+
+        if isinstance(centers, numbers.Integral):
+            n_centers = centers
+
+            # Uniform distribution
+            centers = rng.uniform(
+                center_box[0], center_box[1], size=(n_centers, 2))
+        else:
+            n_features = centers.shape[1]
+            n_centers = centers.shape[0]
+
+    # n_samples is an array indicating the numbers of samples per cluster.
+    else:
+        n_centers = len(n_samples)
+        if centers is None:
+            centers = rng.uniform(
+                center_box[0], center_box[1], size=(n_centers, n_features)
+            )
+        try:
+            assert len(centers) == n_centers
+        except TypeError as e:
+            raise ValueError(
+                "Parameter `centers` must be array-like. Got {!r} instead".format(
+                    centers
+                )
+            ) from e
+        except AssertionError as e:
+            raise ValueError(
+                "Length of `n_samples` not consistent with number of "
+                f"centers. Got n_samples = {n_samples} and centers = {centers}"
+            ) from e
+
+        else:
+            n_features = centers.shape[1]
+
+    # stds: if cluster_std is given as list, it must be consistent
+    # with the n_centers
+    if hasattr(cluster_std, "__len__") and len(cluster_std) != n_centers:
+        raise ValueError(
+            "Length of `clusters_std` not consistent with "
+            "number of centers. Got centers = {} "
+            "and cluster_std = {}".format(centers, cluster_std)
+        )
+
+    # If the cluster standard deviation is fixed for all the clusters create an array of the same
+    # value repeated len(centers) times i.e. the number of clusters.
+    if isinstance(cluster_std, numbers.Real):
+        cluster_std = np.full(len(centers), cluster_std)
+
+    X = []
+    y = []
+
+    if isinstance(n_samples, Iterable):
+        n_samples_per_center = n_samples
+    else:
+        n_samples_per_center = [(n_samples // n_centers)] * n_centers
+
+        # Add the remainder
+        for i in range(n_samples % n_centers):
+            n_samples_per_center[i] += 1
+
+    for i in range(len(cluster_std)):
+        n = n_samples_per_center[i]
+        std = cluster_std[i]
+        # Generate array of size n * n_features
+        # where n is the number of points and n_features is the number if dimensions in the space (2D,3D,4D,...)
+        X.append(rng.normal(loc=centers[i], scale=std, size=(n, n_features)))
+        y += [i] * n
+
+    y = np.array(y)
+
+    # X = np.concatenate(X)
+    # if return_centers:
+    #     return X, y, centers
+    # return X, y
+
+    if return_centers:
+        return X, y, centers
+    return X, y
+
+
+# https://docs.scipy.org/doc/scipy/reference/generated/scipy.spatial.KDTree.html
+# http://www.cemyuksel.com/research/sampleelimination/sampleelimination.pdf
+# https://pypi.org/project/miniball/
+
+class Sample:
+    def __init__(self, coords, neighbors=[], weight=0, index=-1) -> None:
+        self.coords = coords
+        self.neighbors = neighbors
+        self.weight = weight
+        self.index = index
+
+    def get_weight(self):
+        return self.weight
+
+    def set_weight(self, w):
+        self.weight = w
+
+    def __lt__(self, other):
+        return self.weight < other.weight
+
+    def __gt__(self, other):
+        return self.weight > other.weight
+
+    def __str__(self) -> str:
+        return "Coords: {}, Weight: {}, Neighbors: {}".format(self.coords, self.weight, len(self.neighbors))
+
+
+def weighted_sample_elimination(S_np):
+
+    desired = len(S_np) // 2
+
+    # Circle containing all the points in S
+    C, r2 = miniball.get_bounding_ball(S_np)
+    # print(C, r2)
+    c_area = r2 * np.pi
+
+    # Build a kd-tree for samples
+    kd = spatial.KDTree(S_np)
+
+    r_max_2d = np.sqrt(c_area/(2*np.sqrt(3)*len(S_np)))
+
+    S = list(map(tuple, S_np))
+
+    for i in range(len(S_np)):
+        if S_np[i][0] != S[i][0] or S_np[i][1] != S[i][1]:
+            raise ValueError
+
+    samples = dict()
+
+    pq = []
+
+    for i, s in enumerate(S):
+        # s_tuple = (s[0], s[1])
+        new_s = Sample(coords=s, index=i)
+        samples[s] = new_s
+        # heapq.heappush(pq, new_s)
+
+    # key == coordinates
+    for key in samples:
+        s = samples[key]
+        neighbors = kd.query_ball_point(s.coords, r_max_2d)
+        s.neighbors = neighbors
+        # print(neighbors)
+        # print(samples[S[neighbors[0]]])
+        for n in neighbors:
+            d_ij = math.dist(s.coords, S[n])
+            d_hat_ij = min(2 * r_max_2d, d_ij)
+            # s.weight += np.power(1-(d_hat_ij/2 * r_max_2d), 8)
+            s.weight -= np.power(1-(d_hat_ij/2 * r_max_2d), 8)
+
+        heapq.heappush(pq, s)
+
+    while len(pq) > desired:
+        s_j = heapq.heappop(pq)
+
+        for s_i in s_j.neighbors:
+            s_i_obj = samples[S[s_i]]
+            s_i_obj.neighbors.remove(s_j.index)
+            # samples[S[s_i]].weight -= s_j.weight
+
+            s_i_obj.weight += s_j.weight
+
+        heapq.heapify(pq)
+
+    return np.array(list(map(lambda x: x.coords, pq)))
---
+++ test_blobs.py
@@ -0,0 +1,113 @@
+# from sklearn.cluster import kmeans_plusplus
+from my_make_blobs import my_make_blobs, weighted_sample_elimination
+import matplotlib.pyplot as plt
+import matplotlib.patches as patches
+import random
+import numpy as np
+
+
+def test_my_blobs():
+
+    # Generate sample data
+    n_samples = 40
+    n_components = 4
+
+    # X, y, centers = my_make_blobs(
+    #     n_samples=n_samples, centers=n_components,  cluster_std=0.9,
+    #     center_box=(-0.0, 10.0), return_centers=True
+    # )
+
+    X, y, centers = my_make_blobs(
+        [10, 100, 20, 50], centers=None,  cluster_std=[0.9, 0.5, 0.2, 0.7],
+        center_box=(-0.0, 10.0), return_centers=True
+    )
+
+    fig, ax = plt.subplots()
+
+    colors = ["#"+''.join([random.choice('ABCDEF0123456789')
+                           for i in range(6)]) for _ in range(n_components)]
+
+    for i in range(len(X)):
+        X[i] = weighted_sample_elimination(X[i])
+
+    for k, col in enumerate(colors):
+        plt.scatter(X[k][:, 0], X[k][:, 1], c=col, marker=".", s=10)
+
+    plt.scatter(centers[:, 0], centers[:, 1], c="b", s=50)
+
+    for j in range(len(X)):
+        ax.add_patch(confidence_ellipse(X[j], centers[j]))
+
+    plt.title("My Blobs")
+    plt.xticks([])
+    plt.yticks([])
+    plt.show()
+
+# http://www.cs.utah.edu/~tch/CS6640F2020/resources/How%20to%20draw%20a%20covariance%20error%20ellipse.pdf
+
+
+def confidence_ellipse(X, center):
+    x = X[:, 0]
+    y = X[:, 1]
+    cov = np.cov(x, y)
+
+    eigenval, eigenvec = np.linalg.eig(cov)
+
+    max_eigenval = np.max(eigenval)
+    min_eigenval = np.min(eigenval)
+
+    eigenval = np.array([[eigenval[0], 0], [0, eigenval[1]]])
+
+    print("Eigenvectors: \n{}\n Eigenvalues: \n{}\n".format(eigenvec, eigenval))
+
+    print("Max eigenval: {}".format(max_eigenval))
+    print("Min eigenval: {}".format(min_eigenval))
+
+    max_eigenvec_ind_c = 0
+    max_eigenval = eigenval[0][0]
+    for r in range(len(eigenval)):
+        for c in range(len(eigenval[0])):
+            if abs(eigenval[r][c]) >= max_eigenval:
+                max_eigenvec_ind_c = c
+                max_eigenval = eigenval[r][c]
+
+    print("Max eigenvector in eigenvectors matrix (column): {}".format(
+        max_eigenvec_ind_c))
+
+    max_eigenvec = eigenvec[:, max_eigenvec_ind_c]
+
+    print("Max eigenvector: {}".format(max_eigenvec))
+
+    if(max_eigenvec_ind_c == 0):
+        min_eigenvec = eigenvec[:, 1]
+    else:
+        min_eigenvec = eigenvec[0, :]
+
+    print("Min eigenvector: ", min_eigenvec)
+
+    angle = np.arctan2(max_eigenvec[1], max_eigenvec[0])
+
+    if angle < 0:
+        angle = angle + 2 * np.pi
+
+    # https://www.scribbr.com/statistics/standard-deviation/#:~:text=the%20empirical%20rule%3F-,The%20empirical%20rule%2C%20or%20the%2068%2D95%2D99.7%20rule,standard%20deviations%20of%20the%20mean.
+    # Number of standard deviations of the mean.
+    # ~68% within 1 std of the mean
+    # ~95% within 2 std of the mean
+    # ~99.7% within 3 std of the mean
+    n_std = 2
+
+    a = np.sqrt(max_eigenval) * n_std * 2
+    b = np.sqrt(min_eigenval) * n_std * 2
+
+    print("Theta: ", angle)
+
+    theta = np.degrees(angle)
+
+    e = patches.Ellipse((center[0], center[1]), width=a, height=b, angle=theta,
+                        edgecolor='blue', linestyle='--', facecolor='none')
+
+    return e
+
+
+test_my_blobs()
